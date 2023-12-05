6862189cc7439081f55a1d1680ec8aeede7382dd
Przemysław Słoma <przemyslawsloma6@gmail.com>
2023-12-05 16:36:02 +0100

    Day 3

---
+++ src/2023/Day3/Day3.cs
@@ -0,0 +1,130 @@
+﻿namespace AdventOfCode2023.Day3
+{
+    public class Day3
+    {
+        private readonly char[,] _input;
+        private readonly int _inputColumnsCount;
+        private readonly int _inputRowsCount;
+
+        public Day3(string[] input)
+        {
+            (_input, _inputRowsCount, _inputColumnsCount) = ConvertInputTo2dCharArray(input);
+        }
+
+        public int Part1()
+        {
+            int Part1SumFunction(char c, int row, int column)
+            {
+                if (char.IsDigit(c) || c == '.')
+                {
+                    return 0;
+                }
+
+                var numbers = GetNumbersAroundThePoint(row, column);
+                return numbers.Aggregate((a, b) => a + b); ;
+            }
+
+            return WalkTrough2dArray(Part1SumFunction);
+        }
+
+        public int Part2()
+        {
+            int Part2SumFunction(char c, int row, int column)
+            {
+                if (c != '*')
+                {
+                    return 0;
+                }
+
+                var numbers = GetNumbersAroundThePoint(row, column).ToArray();
+                return numbers.Length > 1 ? numbers.Aggregate((a, b) => a * b) : 0;
+
+            }
+
+            return WalkTrough2dArray(Part2SumFunction);
+        }
+
+        private int WalkTrough2dArray(Func<char, int, int, int> functionToExecute)
+        {
+            var sum = 0;
+
+            for (var i = 0; i < _inputRowsCount; i++)
+            {
+                for (var j = 0; j < _inputColumnsCount; j++)
+                {
+                    sum += functionToExecute(_input[i, j], i, j);
+                }
+            }
+
+            return sum;
+        }
+
+        private IEnumerable<int> GetNumbersAroundThePoint(int characterRowIndex, int characterColumnIndex)
+        {
+            var minRowIndex = characterRowIndex <= 0 ? 0 : characterRowIndex - 1;
+            var maxRowIndex = characterRowIndex >= _inputRowsCount - 1 ? _inputRowsCount - 1 : characterRowIndex + 1;
+            var minColIndex = characterColumnIndex <= 0 ? 0 : characterColumnIndex - 1;
+            var maxColIndex = characterColumnIndex >= _inputColumnsCount - 1 ? _inputColumnsCount - 1 : characterColumnIndex + 1;
+
+            for (var rowIndex = minRowIndex; rowIndex <= maxRowIndex; rowIndex++)
+            {
+                var rowNumbers = new List<int>();
+                for (var colIndex = minColIndex; colIndex <= maxColIndex; colIndex++)
+                {
+                    if (char.IsDigit(_input[rowIndex, colIndex]))
+                    {
+                        var number = ReadWholeNumberFrom2dArrayPoint(rowIndex, colIndex);
+                        if (!rowNumbers.Contains(number) || !char.IsDigit(_input[rowIndex, colIndex - 1])) // avoid same values in the same row if there is no separator between
+                        {
+                            rowNumbers.Add(number);
+                            yield return number;
+                        }
+                    }
+                }
+            }
+        }
+
+        private int ReadWholeNumberFrom2dArrayPoint(int numberRowIndex, int numberColumndIndex)
+        {
+            var outputString = $"{_input[numberRowIndex, numberColumndIndex]}";
+            var searchColumnIndex = numberColumndIndex - 1;
+
+            while (searchColumnIndex >= 0 && char.IsDigit(_input[numberRowIndex, searchColumnIndex]))
+            {
+                outputString = _input[numberRowIndex, searchColumnIndex] + outputString;
+                searchColumnIndex--;
+            }
+
+            searchColumnIndex = numberColumndIndex + 1;
+            while (searchColumnIndex < _inputColumnsCount && char.IsDigit(_input[numberRowIndex, searchColumnIndex]))
+            {
+                outputString += _input[numberRowIndex, searchColumnIndex];
+                searchColumnIndex++;
+            }
+
+            return int.Parse(outputString);
+        }
+
+        private static (char[,] result, int rowsCount, int columnsCount) ConvertInputTo2dCharArray(string[] input)
+        {
+            var rowsCount = input.Length;
+            if (rowsCount == 0)
+            {
+                return (new char[0, 0], 0, 0);
+            }
+
+            var columnsCount = input.First().Length;
+            var char2d = new char[rowsCount, columnsCount];
+
+            for (var i = 0; i < rowsCount; i++)
+            {
+                for (var j = 0; j < columnsCount; j++)
+                {
+                    char2d[i, j] = input[i][j];
+                }
+            }
+
+            return (char2d, rowsCount, columnsCount);
+        }
+    }
+}
---
+++ tests/2023/Day3_Tests.cs
@@ -0,0 +1,70 @@
+﻿using AdventOfCode2023.Day3;
+
+namespace AdventOfCode.Tests._2023
+{
+    public class Day3_Tests
+    {
+        [Theory]
+        [InlineData(64, new[] 
+        { // 10 + 20 + 30 + 4
+            "20..15..*",
+            ".30....10",
+            "20%.4&.11"
+        })]
+        [InlineData(16, new[] 
+        {  // 2 + 2 + 1 + 11
+            "*1..",
+            "11.2",
+            "..2%"
+        })]
+        [InlineData(200, new[] 
+        { // 100 + 100
+            "100.100",
+            "...*..." 
+        })]
+        [InlineData(0, new[] 
+        {
+            "1.457..34.",
+            "11..45..23",
+            "..2..234.."
+        })]
+        public void Part1_Tests(int expected, string[] input)
+        {
+            var sut = new Day3(input);
+            var result = sut.Part1();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(620, new[] 
+        { // (2 * 10) + (20 * 30)
+            "20..15.2*",
+            ".30....10",
+            "20*.4*.11"})]
+        [InlineData(5, new[] 
+        { // (1 * 1) + (2 * 2)
+            "*1..",
+            "1..2",
+            "..2*"
+        })]
+        [InlineData(10000, new[] 
+        { // 100 * 100
+            "100.100",
+            "...*..." 
+        })]
+        [InlineData(0, new[]
+        {
+            "*1.....23*",
+            "..324*.1..",
+            "1000......*"
+        })]
+        public void Part2_Tests(int expected, string[] input)
+        {
+            var sut = new Day3(input);
+            var result = sut.Part2();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
