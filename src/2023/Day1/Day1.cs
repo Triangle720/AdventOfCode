22d85fb9e007b4d95c52de5f302d88600882f3ae
Przemysław Słoma <przemyslawsloma6@gmail.com>
2023-12-05 16:35:38 +0100

    Day 1

---
+++ src/2023/Day1/Day1.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode2023.Day1
+{
+    public class Day1
+    {
+        private readonly string[] _input;
+
+        public Day1(string[] input) => _input = input;
+
+        public int Part1()
+        {
+            return _input
+                .Select(x => x.Where(char.IsDigit))
+                .Where(x => x.Any())
+                .Select(x => (x.First() - 48) * 10 + (x.Last() - 48))
+                .Sum();
+        }
+
+        public int Part2()
+        {
+            var digitsWithValues = new Dictionary<string, int>
+            {
+                { "zero", 0 }, { "0", 0 },
+                { "one", 1 }, { "1", 1 },
+                { "two", 2 }, { "2", 2 },
+                { "three", 3 }, { "3", 3 },
+                { "four", 4 }, { "4", 4 },
+                { "five", 5 }, { "5", 5 },
+                { "six", 6 }, { "6", 6 },
+                { "seven", 7 } ,{ "7", 7 },
+                { "eight", 8 }, { "8", 8 },
+                { "nine", 9 }, { "9", 9 }
+            };
+
+            return _input
+                .Select(x =>
+                {
+                    var firstNumIndexes = digitsWithValues.Keys
+                        .Select(y => x.IndexOf(y))
+                        .ToList();
+
+                    var lastNumIndexes = digitsWithValues.Keys
+                        .Select(y => x.LastIndexOf(y))
+                        .ToList();
+
+                    return 
+                          (firstNumIndexes.IndexOf(firstNumIndexes.Where(y => y > -1).Min()) / 2) * 10 
+                        + (lastNumIndexes.IndexOf(lastNumIndexes.Max()) / 2);
+                })
+                .Sum();
+        }
+    }
+}
---
+++ tests/2023/Day1_Tests.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode2023.Day1;
+
+namespace AdventOfCode.Tests._2023
+{
+    public class Day1_Tests
+    {
+        [Theory]
+        [InlineData("1", 11)]
+        [InlineData("32", 32)]
+        [InlineData("5abc2", 52)]
+        [InlineData("1abc4d", 14)]
+        [InlineData("a9dse5", 95)]
+        [InlineData("u8ekf6dwq", 86)]
+        [InlineData("63423634124635", 65)]
+        public void Part1_Tests(string input, int expected)
+        {
+            var sut = new Day1([input]);
+            var result = sut.Part1();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("one", 11)]
+        [InlineData("threetwo", 32)]
+        [InlineData("fiveabctwo", 52)]
+        [InlineData("oneabcfourd", 14)]
+        [InlineData("aninedsefive", 95)]
+        [InlineData("ueightekfsixdwq", 86)]
+        [InlineData("sixthreefourtwothreesixthreefouronetwofoursixthreefive", 65)]
+        [InlineData("1two", 12)]
+        [InlineData("seven5", 75)]
+        [InlineData("6eightseven6", 66)]
+        public void Part2_Tests(string input, int expected)
+        {
+            var sut = new Day1([input]);
+            var result = sut.Part2();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
