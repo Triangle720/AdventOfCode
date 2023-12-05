c54c5b558d756b2f9ede9219e0433d406745580d
Przemysław Słoma <przemyslawsloma6@gmail.com>
2023-12-05 16:35:51 +0100

    Day 2

---
+++ src/2023/Day2/Day2.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode2023.Day2
+{
+    public class Day2
+    {
+        private readonly string[] _input;
+
+        public Day2(string[] input) => _input = input;
+
+        public int Part1()
+        {
+            return _input
+                .Select((line, index) => (Draws: ReadCubesDrawsFromInputLine(line), Index: index + 1))
+                .Where(x => x.Draws.All(y => y.IsPossible()))
+                .Sum(x => x.Index);
+        }
+
+        public int Part2()
+        {
+            return _input.Sum(x => {
+                var draws = ReadCubesDrawsFromInputLine(x);
+                return draws.Max(x => x.RedCubes) * draws.Max(x => x.GreenCubes) * draws.Max(x => x.BlueCubes);
+            });
+        }
+
+        private record CubesDraw(int RedCubes, int GreenCubes, int BlueCubes)
+        {
+            public bool IsPossible()
+            {
+                return RedCubes <= 12 && GreenCubes <= 13 && BlueCubes <= 14;
+            }
+        }
+
+        private static CubesDraw[] ReadCubesDrawsFromInputLine(string line)
+        {
+            var draws = line[(line.IndexOf(':') + 1)..].Split(';');
+
+            return draws.Select(x =>
+            {
+                var countAndColors = x
+                        .Split(',')
+                        .Select(x => x.TrimStart().Split(' '))
+                    .ToDictionary(pair => pair[1], pair => int.Parse(pair[0]));
+
+                return new CubesDraw(// 1 by default, legal move, it has no impact on multiplication in Pt. 2
+                    countAndColors.TryGetValue("red", out int red) ? red : 1,
+                    countAndColors.TryGetValue("green", out int green) ? green : 1,
+                    countAndColors.TryGetValue("blue", out int blue) ? blue : 1);
+            }).ToArray();
+        }
+    }
+}
---
+++ tests/2023/Day2_Tests.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode2023.Day2;
+
+namespace AdventOfCode.Tests._2023
+{
+    public class Day2_Tests
+    {
+        [Theory]
+        [MemberData(nameof(Part1_Test_Input))]
+        public void Part1_Tests(string[] input, int expected)
+        {
+            var sut = new Day2(input);
+            var result = sut.Part1();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("Game 1: 1 red; 1 green; 1 blue", 1)]
+        [InlineData("Game 1: 20 red; 20 green; 20 blue", 8000)]
+        [InlineData("Game 1: 10 red, 10 green, 10 blue; 5 green, 15 red; 15 green 5 blue", 2250)]
+        [InlineData("Game 1: 20 red; 20 green", 400)]
+        public void Part2_Tests(string input, int expected)
+        {
+            var sut = new Day2([input]);
+            var result = sut.Part2();
+
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> Part1_Test_Input()
+        {
+            yield return new object[] { new[] { "Game 1: 1 red; 1 green; 1 blue" }, 1 };
+            yield return new object[] { new[] { "Game 1: 12 red; 13 green; 14 blue" }, 1 };
+            yield return new object[] { new[] { "Game 1: 13 red; 14 green; 15 blue" }, 0 };
+            yield return new object[]
+            {
+                new []
+                {
+                    "Game 1: 1 red, 1 green, 1 blue; 12 red, 13 green, 14 blue",
+                    "Game 2: 10 red, 10 blue, 10 green; 1 red; 12 green",
+                    "Game 3: 4 blue, 6 green; 20 red"
+                }, 3
+            };
+        }
+    }
+}
