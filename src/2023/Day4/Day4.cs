c7a4243fc533144f89e4e3278c80963af3eb451c
Przemysław Słoma <przemyslawsloma6@gmail.com>
2023-12-05 16:36:10 +0100

    Day 4

---
+++ src/2023/Day4/Day4.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode2023.Day4
+{
+    public class Day4
+    {
+        private readonly string[] _input;
+
+        public Day4(string[] input) => _input = input;
+
+        public int Part1()
+        {
+            return _input.Select(x =>
+            {
+                var matchCount = GetMatchCountFromLine(x);
+                return matchCount > 0 ? (int)Math.Pow(2, matchCount - 1) : 0;
+            }).Sum();
+        }
+
+        public int Part2()
+        {
+            var sratchardIndexesAndCount = Enumerable.Range(1, _input.Length).ToDictionary(x => x, y => 1);
+
+            for (var i = 0; i < _input.Length; i++)
+            {
+                var matchCount = GetMatchCountFromLine(_input[i]);
+                
+                for (var j = 1; j <= matchCount; j++)
+                {
+                    var scratchcardIndex = i + j + 1;
+                    if (sratchardIndexesAndCount.ContainsKey(scratchcardIndex))
+                    {
+                        sratchardIndexesAndCount[scratchcardIndex] += sratchardIndexesAndCount[i + 1];
+                    }         
+                }
+            }
+  
+            return sratchardIndexesAndCount.Sum(x => x.Value);
+        }
+
+        private static int GetMatchCountFromLine(string line)
+        {
+            var winningAndInNumbers = line[(line.IndexOf(':') + 1)..].Split(" | ");
+
+            int[] ReadNumbersFromSection(string @string)
+            {
+                return @string.Split(' ')
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Select(int.Parse)
+                .ToArray();
+            };
+
+            return ReadNumbersFromSection(winningAndInNumbers[1]).Count(ReadNumbersFromSection(winningAndInNumbers[0]).Contains);
+        }
+    }
+}
---
+++ tests/2023/Day4_Tests.cs
@@ -0,0 +1,51 @@
+﻿using AdventOfCode2023.Day4;
+
+namespace AdventOfCode.Tests._2023
+{
+    public class Day4_Tests
+    {
+        [Theory]
+        [InlineData(13, new[]
+        {
+            "Card 1: 10 20 30 40 | 10 20 30 40", // 2^3 = 8p
+            "Card 2:  5  4  3  2 |  2  3  4  6", // 2^2 = 4p
+            "Card 3:  1 |  1"                    // 1 = 1 p
+        })]
+        [InlineData(0, new[]
+        {
+            "Card 1: 10 20 30 40 | 50 60 70 80", // 0p
+            "Card 2:  1  2  3  4 |  5  6  7  8", // 0p
+            "Card 3:  1 |  2"                    // 0 p
+        })]
+        public void Part1_Tests(int expected, string[] input)
+        {
+            var sut = new Day4(input);
+            var result = sut.Part1();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(31, new[]                    // cards count
+        {                                        // 1 1 1 1 1
+            "Card 1: 10 20 30 40 | 10 20 30 40", // 1 2 2 2 2
+            "Card 2:  1  2  3  4 |  1  2  3  4", // 1 2 4 4 4
+            "Card 3:  3 30 49 | 11 49 30",       // 1 2 4 8 8
+            "Card 4:  1 10 20 | 21 20 25",       // 1 2 4 8 16
+            "Card 5: 10 11 12 13 | 13 12 11 10"  // sum:31
+        })]
+        [InlineData(3, new[]
+        {                                        // 1 1 1
+            "Card 1: 10 20 30 40 | 50 60 70 80", // 1 1 1 (no matches) 
+            "Card 2:  1  2  3  4 |  5  6  7  8", // 1 1 1 (no matches) 
+            "Card 3:  1 |  2"                    // sum:3 (only original cards)
+        })]
+        public void Part2_Tests(int expected, string[] input)
+        {
+            var sut = new Day4(input);
+            var result = sut.Part2();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
